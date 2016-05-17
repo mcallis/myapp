1effbe8ac808c3055ebf0e09df81e2f5f20f507b
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-18 01:12:10 +0200

    Check usersession when autologgedin

--- myapp/TabBar.swift
+++ myapp/TabBar.swift
@@ -11,7 +11,26 @@ import Foundation
 class TabBar: UITabBarController{
     
     override func viewDidLoad() {
-       
+        checkSession()
+    }
+    
+    
+    func checkSession(){
+        let mAppManager = AppManager.sharedInstance
+        if (mAppManager.autoLoggedIn != nil && mAppManager.autoLoggedIn) {
+            UserSession.get()
+            let name = UserSession.userName
+            let pass = UserSession.password
+            
+            
+            mAppManager.doLogin(name, password: pass) { (success, error) in
+                if !success{
+                    // Show alert
+                    let loginVC = self.storyboard?.instantiateViewControllerWithIdentifier("LoginViewController") as! Login
+                    self.presentViewController(loginVC, animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     
