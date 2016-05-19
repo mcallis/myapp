6b87a6d79711659cc886dc42c4f81cb5402c1719
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-19 03:14:50 +0200

    PAC1 Set true textFieldShouldReturn

--- myapp/Login.swift
+++ myapp/Login.swift
@@ -43,7 +43,7 @@ class Login: UITableViewControllerOwn, UITextFieldDelegate {
         default:
             tapLogin()
         }
-        return false
+        return true
     }
     
     @IBAction func tapLogin() {
