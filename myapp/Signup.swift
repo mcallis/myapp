208889906603f3c392a4bc968c35c32a090af116
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-19 02:41:03 +0200

    PAC1 Update signup viewcontroller

--- myapp/Signup.swift
+++ myapp/Signup.swift
@@ -10,24 +10,30 @@ import UIKit
 
 
 
-class Signup: UIViewControllerOwn, UITextFieldDelegate {
+class Signup: UITableViewControllerOwn, UITextFieldDelegate {
     
-    @IBOutlet weak var fieldUsername: UITextField!
-    @IBOutlet weak var fieldPassword: UITextField!
-    @IBOutlet weak var fieldName: UITextField!
-    @IBOutlet weak var fieldEmail: UITextField!
+    @IBOutlet weak var fieldUsername: UITextFieldOwn!
+    @IBOutlet weak var fieldPassword: UITextFieldOwn!
+    @IBOutlet weak var fieldConfirmPassword: UITextFieldOwn!
+    @IBOutlet weak var fieldName: UITextFieldOwn!
+    @IBOutlet weak var fieldEmail: UITextFieldOwn!
+    @IBOutlet weak var btnRegister: UIBarButtonItem!
+    @IBOutlet weak var fieldAcceptTerms: UISwitch!
+  
+    
+
+
     
     let mAppManager: AppManager = AppManager.sharedInstance
     var fromLogOut: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = btnRegister
         self.navigationItem.title = Constants.TitlesViews.signup
+        fieldAcceptTerms.on = false
     }
     
-    @IBAction func closeView(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
     
     @IBAction func signUp(sender: AnyObject) {
         self.startIndicator()
@@ -51,19 +57,43 @@ class Signup: UIViewControllerOwn, UITextFieldDelegate {
     }
     
     func canDoSignUp() -> Bool{
+        var errors = [String]()
+        let acceptTerms = fieldAcceptTerms.on
+        
         if fieldUsername.text!.isEmpty{
-            self.alertError(Constants.ErrorMessage.userNameisRequired)
-            return false
+            errors.append(Constants.ErrorMessage.userNameisRequired)
         }
         if fieldPassword.text!.isEmpty{
-            self.alertError(Constants.ErrorMessage.passwordisRequired)
-            return false
+            errors.append(Constants.ErrorMessage.passwordisRequired)
+        }
+        if fieldPassword.text != fieldConfirmPassword.text {
+            errors.append(Constants.ErrorMessage.passwordsDontMatch)
         }
         if fieldName.text!.isEmpty{
-            self.alertError(Constants.ErrorMessage.nameisRequired)
-            return false
+            errors.append(Constants.ErrorMessage.nameisRequired)
         }
-        return true
+        
+        if fieldEmail.text!.isEmpty {
+            errors.append(Constants.ErrorMessage.emailisRequired)
+        }
+        
+        if (acceptTerms == false) {
+            errors.append(Constants.ErrorMessage.mustAcceptTerms)
+        }
+        
+        let numErrors = errors.count
+        if numErrors > 0 {
+            // Informamos al usuario
+            let errorMessage = errors.joinWithSeparator(".\n")
+            let alertController = UIAlertController(title: "Review the registration form", message: errorMessage, preferredStyle: .Alert)
+            let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
+            alertController.addAction(OKAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+            return false
+        } else {
+            return true
+        }
+        
     }
     
     func toMainApp(){
