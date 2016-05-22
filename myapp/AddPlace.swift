90dbbc9e66a6195b5c20e1161ec80fc455c229e2
Marc Callís Vilalta <callis.marc@gmail.com>
2016-05-22 02:37:28 +0200

    PAC2 Add add place

---
+++ myapp/AddPlace.swift
@@ -0,0 +1,21 @@
+//
+//  AddPlace.swift
+//  myapp
+//
+//  Created by Marc Callís Vilalta on 22/5/16.
+//  Copyright © 2016 uoc. All rights reserved.
+//
+
+import Foundation
+
+class AddPlace: UITableViewControllerOwn {
+    
+    let mPlaceManager = PlaceManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
